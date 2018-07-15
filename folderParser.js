c7e656bbb7a1a6473eecb72a3ee26251536c5eb6
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-07-15 11:38:42 -0500

    Check if path valid

--- folderParser.js
+++ folderParser.js
@@ -1,4 +1,11 @@
+const fs = require("fs");
+
 const folderParser = apiBasePath => {
+  if (!fs.existsSync(apiBasePath)) {
+    console.error(`apiBasePath "${apiBasePath}" not found`);
+    process.exit(1);
+  }
+
   return {
     apiBasePath: apiBasePath
   };
