d70783efa7dda7f55de96fa6642fbc22754c902a
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-07-15 11:12:57 -0500

    Add command-line arguments and help

--- mulint.js
+++ mulint.js
@@ -2,6 +2,19 @@
 
 const program = require("commander");
 
-program.version("0.0.1").description("Mule project linter");
-
-program.parse(process.argv);
+program
+  .version("0.0.1")
+  .description("Mule project linter")
+  .arguments("<apiBasePath>")
+  .on("--help", () => {
+    const path = "C:\\SourceCode\\mulesoft-apis\\System APIs\\wsflx-system-api";
+    console.log("");
+    console.log("  Example:");
+    console.log("");
+    console.log(`    mulint "${path}"`);
+    console.log("");
+  })
+  .action(apiBasePath => {
+    console.log(apiBasePath);
+  })
+  .parse(process.argv);
