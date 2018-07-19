42276d25eaaa0c12a7f6d3aa1a5c04c2472599d9
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-07-18 20:24:29 -0500

    Put quotes around expected/actual values

--- assert.js
+++ assert.js
@@ -2,7 +2,7 @@ const failures = [];
 
 const equals = (expected, actual, context) => {
   if (actual !== expected) {
-    failures.push(`${context}: expected ${expected} but was ${actual}`);
+    failures.push(`${context}: expected "${expected}" but was "${actual}"`);
   }
 };
 
