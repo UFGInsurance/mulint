6a158d12c58cd80be17dceb1dbc3a2c94919cd3d
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-07-19 12:50:16 -0500

    Add POM flowref property check

--- validatePom.js
+++ validatePom.js
@@ -4,6 +4,7 @@ const domainProjectName = "api-gateway";
 const expectedDomainProjectVersion = "1.0.1";
 const expectedGroupId = "com.unitedfiregroup";
 const expectedVersion = "1.0.0";
+const expectedFlowRef = "${project.artifactId}-main";
 
 const validatePom = (folderInfo, pomInfo) => {
   let isOnPrem = pomInfo.properties.get("deployment.type") === "arm";
@@ -38,6 +39,12 @@ const validatePom = (folderInfo, pomInfo) => {
   );
 
   assert.equals(expectedVersion, pomInfo.xml.project.version[0], "POM version");
+
+  assert.equals(
+    expectedFlowRef,
+    pomInfo.properties.get("api.manager.flowref"),
+    "POM api.manager.flowref"
+  );
 };
 
 module.exports = validatePom;
