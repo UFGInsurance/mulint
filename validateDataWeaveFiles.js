e737cc89f47705b044980e64112384626ccfdebf
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-07-21 18:32:13 -0500

    Add check for unused common library variable

--- validateDataWeaveFiles.js
+++ validateDataWeaveFiles.js
@@ -6,6 +6,7 @@ const assert = require("./assert");
 // Possible false positive - might be line comment
 // as opposed to commented-out code.
 const commentedOutLineRegEx = /^\s*\/\//;
+const commonLibraryVariableDeclarationRegEx = /^\s*%var\s+(\S+)\s*=\s*readUrl.+common-formatting-lib/;
 
 const validateDataWeaveFiles = folderInfo => {
   folderInfo.dataWeaveFiles.forEach(dataWeaveFile => {
@@ -17,14 +18,44 @@ const validateDataWeaveFiles = folderInfo => {
       .split(os.EOL);
 
     let isCommentedOutLine = false;
+    let commonLibraryVariable = null;
+    let commonLibraryVariableUseRegEx = null;
+    let isCommonLibraryVariableUsed = false;
 
     lines.forEach(line => {
       if (commentedOutLineRegEx.test(line)) {
         isCommentedOutLine = true;
+      } else {
+        // Not a comment
+        if (!commonLibraryVariable) {
+          let match = commonLibraryVariableDeclarationRegEx.exec(line);
+
+          if (match) {
+            commonLibraryVariable = match[1];
+            commonLibraryVariableUseRegEx = new RegExp(
+              `${commonLibraryVariable}\\.valueAs`
+            );
+          }
+        } else {
+          // Common library variable has been declared
+          if (
+            !isCommonLibraryVariableUsed &&
+            commonLibraryVariableUseRegEx.test(line)
+          ) {
+            isCommonLibraryVariableUsed = true;
+          }
+        }
       }
     });
 
     assert.isTrue(!isCommentedOutLine, `${context}: commented-out line found`);
+
+    if (commonLibraryVariable) {
+      assert.isTrue(
+        isCommonLibraryVariableUsed,
+        `${context}: common library variable "${commonLibraryVariable}" declared but not used`
+      );
+    }
   });
 };
 
