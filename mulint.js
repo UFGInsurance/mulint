d6f43a1a9aa6ed2ae15497921ce378b9e850eaa8
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-09-11 10:41:51 -0500

    Version 1.2.0

--- mulint.js
+++ mulint.js
@@ -15,7 +15,7 @@ const validateDataWeaveFiles = require("./validateDataWeaveFiles");
 const assert = require("./assert");
 
 program
-  .version("1.1.1")
+  .version("1.2.0")
   .description("Mule project linter")
   .arguments("<apiBasePath>")
   .on("--help", () => {
