3d6f18f04f1ca636abc5d0728b9aba24485e9a9f
Bill Sorensen <wsorensen@unitedfiregroup.com>
2018-11-02 10:22:29 -0500

    Fix read file encoding on validateGitignore

--- validateGitignore.js
+++ validateGitignore.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const assert = require("./assert");
+const { encoding } = require("./constants");
 
 const validateGitignore = folderInfo => {
-  let contents = fs.readFileSync(folderInfo.gitignoreFile).toString();
+  let contents = fs.readFileSync(folderInfo.gitignoreFile, encoding);
 
   assert.isTrue(
     /^\.project\s*$/m.test(contents),
