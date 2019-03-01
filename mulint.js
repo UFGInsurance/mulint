48aaa2eaae28d290e621e387aa922cc05d6e49c4
Bill Sorensen <wsorensen@unitedfiregroup.com>
2019-03-01 16:42:58 -0600

    Now assumes on-prem if no Mule Maven Plugin.
    Version 2.0.0 (technically breaking change).

--- mulint.js
+++ mulint.js
@@ -14,7 +14,7 @@ const validateLog4j = require("./validateLog4j");
 const assert = require("./assert");
 
 program
-  .version("1.9.0")
+  .version("2.0.0")
   .description("Mule project linter")
   .arguments("<apiBasePath>")
   .on("--help", () => {
